8f15699b817d88197e116ec4a37fbaf8d9469d3a
abduldardar <olivierlagardemail@gmail.com>
2025-11-04 00:29:34 +0100

    Update script.js
    articles1

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function handleImageError(img) {
 // Chargement des articles
 async function loadArticles() {
     try {
-        const response = await fetch('./data/articles4.json');
+        const response = await fetch('./data/articles.json');
         if (!response.ok) {
             throw new Error('Erreur lors du chargement des articles');
         }
